aa98aa109cbd5eb184998479e24e9b7621ae311f
psrobotics <psrobotics@foxmail.com>
2023-03-28 14:00:54 -0700

    add: 0328 openloop traj

--- quad_walk_ctr/main.m
+++ quad_walk_ctr/main.m
@@ -12,8 +12,28 @@ import casadi.*;
 addpath('math\');
 addpath('srb_dynamics\');
 addpath('mpc_controller\');
+addpath('fpp_planner\');
+addpath('visualization\');
 
 %% Get dynamics
 [dyn_f] = get_srb_dynamics(world_params, body_params, path);
 
-[mpc_v, mpc_c, mpc_p] = form_mpc_prob(path, world_params, body_params, ctr_params, dyn_f)
+%% Form the mpc problem
+[mpc_v, mpc_c, mpc_p] = form_mpc_prob(world_params, body_params, ctr_params, dyn_f, path);
+
+[boundray_v] = add_state_boundaries(mpc_v, mpc_c, world_params, body_params, ctr_params, path);
+
+[ref_traj_v] = fpp_planner(world_params, body_params, ctr_params, path);
+
+%% Slove the NLP prob
+sol = mpc_p.solver('x0',ref_traj_v.x0,...
+                   'lbx',boundray_v.lbx,...
+                   'ubx',boundray_v.ubx,...
+                   'lbg',boundray_v.lbg,...
+                   'ubg',boundray_v.ubg,...
+                   'p',ref_traj_v.p);
+               
+  %% Unpack data
+  [x_sol, f_sol, fp_l_sol, fp_g_sol] = unpacks_sol(sol, body_params, ctr_params, path);
+  
+  rbt_anime(x_sol,f_sol,fp_g_sol,[],ctr_params.T,ctr_params.N);
